2e2cb399d9d75d810a8d36442ede2f87f11d2247
kyuriev <kyuriev@cspmz.ru>
2020-09-10 17:19:02 +0300

    complete packer image for CentOs 8 Load balancer

--- kvm/main.tf
+++ kvm/main.tf
@@ -1,23 +1,4 @@
 
-################################################################################
-# ENV VARS
-################################################################################
-
-# https://www.terraform.io/docs/commands/environment-variables.html
-variable "VM_COUNT" {
-  default = 3
-  type = number
-}
-
-variable "VM_HOSTNAME" {
-  default = "vm"
-  type = string
-}
-
-variable "VM_USER" {
-  default = "kyuriev"
-  type = string
-}
 
 ################################################################################
 # PROVIDERS
@@ -38,12 +19,83 @@ provider "libvirt" {
 ################################################################################
 # RESOURCES
 ################################################################################
+resource "libvirt_pool" "default" {
+  name = var.libvirt_storage_pool_name
+  type = "dir"
+  path = var.libvirt_storage_pool_path
+}
+#### Define Centos 8 packer image
+resource "libvirt_volume" "okd4-services-qcow2" {
+  name = "okd4-services.qcow2"
+  pool = libvirt_pool.default.name
+  source = "../packer/centOsKvm/output-qemu/fedoraCoreOs-packer.qcow2"
+  format = "qcow2"
 
+  provisioner "local-exec" {
+     command = <<EOF
+       sudo qemu-img resize "${var.libvirt_storage_pool_path}/${libvirt_volume.okd4-services-qcow2.name}" "${var.okd4-services_disk}"G   # TODO remove sudo
+       EOF
+   }
+}
+
+resource "libvirt_domain" "okd4-services" {
+  name   = "${var.cluster_name}-sevices"
+  memory = var.okd4-services_memory
+  vcpu   = var.okd4-services_vcpu
+
+
+  network_interface {
+    network_name = "default"
+    hostname       = "${var.cluster_name}-services.${var.network_domain}"
+    addresses      = [cidrhost(var.network_address,2)]
+    wait_for_lease = true
+
+  }
+
+  boot_device {
+    dev = [ "hd", "cdrom"]
+  }
+
+
+  # IMPORTANT: this is a known bug on cloud images, since they expect a console
+  # we need to pass it
+  # https://bugs.launchpad.net/cloud-images/+bug/1573095
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.okd4-services-qcow2.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+
+  provisioner "local-exec" {
+     command = <<EOF
+        ansible-playbook -i ./ansible/inventory ./ansible/tasks/haproxy_config.yml -vvv
+       EOF
+   }
+
+}
+
+#### Define and deploy CoreOs
 resource "libvirt_volume" "fedoraCoreOs_image" {
   name = "fedoraCoreOs_image"
-  pool = "default"
+  pool = libvirt_pool.default.name
   #source = "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/32.20200824.3.0/x86_64/fedora-coreos-32.20200824.3.0-qemu.x86_64.qcow2.xz"
-  source = "../packer/output-qemu/fedoraCoreOs-packer.qcow2"
+  source = "../packer/fedoraCoreOsKvm/output-qemu/fedoraCoreOs-packer.qcow2"
   format = "qcow2"
 }
 
@@ -53,7 +105,10 @@ resource "libvirt_volume" "os_volume" {
   count          = var.VM_COUNT
 }
 
-# Define KVM domain to create
+### Deploy virtual machines
+#### Deploy load balancer
+
+#### Deploy working nodes
 resource "libvirt_domain" "vm" {
   count  = var.VM_COUNT
   name   = "${var.VM_HOSTNAME}-${count.index}"
---
+++ kvm/variable.tf
@@ -0,0 +1,59 @@
+
+################################################################################
+# ENV VARS
+################################################################################
+
+# https://www.terraform.io/docs/commands/environment-variables.html
+variable "VM_COUNT" {
+  default = 3
+  type = number
+}
+
+variable "VM_HOSTNAME" {
+  default = "vm"
+  type = string
+}
+
+variable "VM_USER" {
+  default = "kyuriev"
+  type = string
+}
+
+
+variable "cluster_name" {
+     default = "okd4"
+}
+
+variable "network_address" {
+     default = "192.168.122.1/24"
+}
+
+variable "network_domain" {
+     default = "cspmz.local"
+  }
+
+### Storage
+variable "libvirt_storage_pool_name" {
+     default = "default"
+}
+
+variable "libvirt_storage_pool_path" {
+     default = "/var/lib/libvirt/images"
+}
+
+### LB
+variable "okd4-services_memory" {
+     description = "GB"
+     default = "4"
+}
+
+variable "okd4-services_vcpu" {
+     default = "4"
+}
+variable "okd4-services_disk" {
+     description = "GB"
+     default = "120"
+}
+variable "lb_vm_volume_source" {
+     default = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-1809.qcow2"
+}
