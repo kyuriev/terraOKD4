542ccedc5c8382743ff42e0d2b3f29f3581e24b9
kyuriev <kyuriev@cspmz.ru>
2020-09-11 16:37:25 +0300

    add cloud-init to ks.cfg

--- kvm/c8TestDeploy/main.tf
+++ kvm/c8TestDeploy/main.tf
@@ -28,18 +28,44 @@ resource "libvirt_volume" "okd4-services-qcow2" {
   format = "qcow2"
 }
 
+# cloudinit
+
+data "template_file" "network_config" {
+ template = file("${path.module}/network_config.cfg")
+}
+
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud_init.cfg")
+}
+
+data "template_file" "network_config" {
+  template = file("${path.module}/network_config.cfg")
+}
+
+# for more info about paramater check this out
+# https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/website/docs/r/cloudinit.html.markdown
+# Use CloudInit to add our ssh-key to the instance
+# you can add also meta_data field
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "commoninit.iso"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+  pool           = "default"
+}
+
 resource "libvirt_domain" "okd4-services" {
   name = "${var.cluster_name}-sevices"
   memory = var.okd4-services_memory
   vcpu = var.okd4-services_vcpu
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
 
 
   network_interface {
     network_name = "default"
-    hostname       = "${var.cluster_name}-services.${var.network_domain}"
-    addresses      = [cidrhost(var.network_address,2)]
-    wait_for_lease = true
-
+    hostname  = "centos8-test"
+    addresses = ["192.168.122.250"]
   }
 
   # IMPORTANT: this is a known bug on cloud images, since they expect a console
@@ -61,9 +87,16 @@ resource "libvirt_domain" "okd4-services" {
     volume_id = libvirt_volume.okd4-services-qcow2.id
   }
 
+
   graphics {
     type        = "spice"
     listen_type = "address"
     autoport    = true
   }
+
+      provisioner "local-exec" {
+      command = <<EOF
+        ansible-playbook -i ../../provisioning/inventory ../../provisioning/okd4-services/main.yml -vvv
+       EOF
+   }
 }
